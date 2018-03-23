f0e121f32e71ae5237e37a2700f42f4abeda0ad3
Amitav Roy <reachme@amitavroy.com>
2018-03-23 11:47:45 +0530

    Work in progress

---
+++ pages/movies/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppSection>
+    <div class="container">
+      <h3 class="title is-h3 has-text-centered"> List of movies </h3>
+
+    </div>
+  </AppSection>
+</template>
+
+<script>
+import AppSection from '~/components/AppSection'
+export default {
+  components: {
+    AppSection
+  }
+}
+</script>
